a31f25350d0705414e999114c657c3e9746e92de
DmitrievRB <merlinrd@yandex.ru>
2024-06-01 17:55:08 +0300

    загрузка

---
+++ pet_shop_sql/DROP TABLE.sql
@@ -0,0 +1,16 @@
+
+DROP TABLE animals;
+DROP TABLE coloring;
+DROP TABLE vaccinations;
+DROP TABLE variety;
+DROP TABLE feed;
+Drop Table valliers;
+DROP TABLE valliers_date;
+DROP TABLE family;
+DROP TABLE log_feed;
+DROP TABLE pets;
+DROP TABLE operations;
+DROP TABLE log_pets;
+DROP VIEW pets_in;
+DROP VIEW pets_out;
+DROP VIEW v ;
